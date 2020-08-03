64fba08600a34828beee751ce819c981a85b93e8
David Griffiths <dogriffiths@gmail.com>
2020-08-03 20:10:50 +0100

    feat: Add recipe for the reducer hook

---
+++ ch03-01-reducer/src/Puzzle.js
@@ -0,0 +1,30 @@
+import React, {useReducer} from "react";
+import reducer from "./reducer";
+
+import "./Puzzle.css";
+
+export default () => {
+    const [state, dispatch] = useReducer(reducer, {
+        items: ['4', '1', '2', '7', '6', '3', null, '5', '8']
+    });
+
+    return <div className='Puzzle'>
+        <div className='Puzzle-squares'>
+            {
+                state.items.map((s, i) => <div
+                    className={`Puzzle-square ${s ? '' : 'Puzzle-square-empty'}`}
+                    key={`square-${i}`}
+                    onClick={() => dispatch({type: 'move', payload: i})}
+                >{s}</div>)
+            }
+        </div>
+        <div className='Puzzle-controls'>
+            <button className='Puzzle-shuffle' onClick={() => dispatch({type: 'shuffle'})}>Shuffle</button>
+            <button className='Puzzle-reset' onClick={() => dispatch({type: 'reset'})}>Reset</button>
+        </div>
+        {
+            state.complete &&
+            <div className='Puzzle-complete'>Complete!</div>
+        }
+    </div>;
+};
---
+++ ch03-01-reducer/src/reducer.js
@@ -0,0 +1,75 @@
+function trySwap(newItems, position, t) {
+    if (newItems[t] === null) {
+        const temp = newItems[position];
+        newItems[position] = newItems[t];
+        newItems[t] = temp;
+    }
+}
+
+function arraysEqual(a, b) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const CORRECT = ['1', '2', '3', '4', '5', '6', '7', '8', null];
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "move": {
+            const position = action.payload;
+            const newItems = [...state.items];
+            const col = position % 3;
+
+            if (position < 6) {
+                trySwap(newItems, position, position + 3);
+            }
+            if (position > 2) {
+                trySwap(newItems, position, position - 3);
+            }
+            if (col < 2) {
+                trySwap(newItems, position, position + 1);
+            }
+            if (col > 0) {
+                trySwap(newItems, position, position - 1);
+            }
+
+            return {
+                ...state,
+                items: newItems,
+                complete: arraysEqual(
+                    newItems,
+                    CORRECT),
+            }
+        }
+        case 'shuffle': {
+            let newState = {...state};
+            do {
+                for (let i = 0; i < 300; i++) {
+                    newState = reducer(
+                        {...newState},
+                        {
+                            type: 'move',
+                            payload: Math.floor(Math.random() * 9)
+                        })
+                }
+            } while (newState.complete);
+            return newState;
+        }
+        case 'reset': {
+            return {
+                ...state,
+                items: [...CORRECT],
+                complete: true,
+            }
+        }
+        default: {
+            throw new Error("Unknown action: " + action.type);
+        }
+    }
+}
+
+export default reducer;
