942cf6c1e71eb7c25b38c0c22fdd5c18151b3c47
David Griffiths <dogriffiths@gmail.com>
2020-10-24 16:53:44 +0100

    feat: Add code for recipe 4-3

--- ch04-03-reducer-seq/src/App.js
+++ ch04-03-reducer-seq/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useReducer, useState} from 'react';
 import './App.css';
+import deletionReducer from "./deletionReducer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(deletionReducer, {});
+    const [password, setPassword] = useState();
+
+    return <div className="App">
+        <button
+            onClick={() => {
+                dispatch({type: 'START_DELETION'});
+            }}
+            disabled={state.deleteButtonDisabled}
+        >Delete Widget!
+        </button>
+        <div className='App-message'>{state.message}</div>
+        {
+            state.showLogin &&
+                <div className='App-dialog'>
+                    <p>Enter your password</p>
+                    <input type='password' value={password} onChange={evt => setPassword(evt.target.value)}/>
+                    <button onClick={() => dispatch({type: 'LOGIN', payload: password})}>Login</button>
+                    <button onClick={() => dispatch({type: 'CANCEL_DELETION'})}>Cancel</button>
+                    <div className='App-error'>{state.loginError}</div>
+                </div>
+        }
+        {
+            state.showConfirmation &&
+                <div className='App-dialog'>
+                    <p>Are you sure you want to delete the widget?</p>
+                    <button onClick={() => dispatch({type: 'CONFIRM_DELETION'})}>Yes</button>
+                    <button onClick={() => dispatch({type: 'CANCEL_DELETION'})}>No</button>
+                </div>
+        }
+        {
+            state.showResult &&
+            <div className='App-dialog'>
+                <p>The widget was deleted</p>
+                <button onClick={() => dispatch({type: 'FINISH'})}>Done</button>
+            </div>
+        }
+    </div>;
 }
 
 export default App;
