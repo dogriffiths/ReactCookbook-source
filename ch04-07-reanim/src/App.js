d615c26ef4596a506e159009792bd998f91b6fb9
David Griffiths <dogriffiths@gmail.com>
2020-10-27 15:38:46 +0000

    feat: Add code for recipe 4-7

--- ch04-07-reanim/src/App.js
+++ ch04-07-reanim/src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {pulse, zoomOut, shake, merge} from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+
 import './App.css';
 
+const styles = {
+    created: {
+        animation: 'x 0.5s',
+        animationName: Radium.keyframes(pulse, 'pulse')
+    },
+    deleted: {
+        animation: 'x 0.5s',
+        animationName: Radium.keyframes(merge(zoomOut, shake), 'zoomOut')
+    },
+};
+
+function getStyleForItem(item) {
+    return item.deleting ? styles.deleted : item.creating ? styles.created : null;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    let deleteItem = (i) => setData(d => {
+        const result = [...d];
+        result[i].deleting = true;
+        return result;
+    });
+    let createItem = () => {
+        setData(d => [...d, {
+            url: `https://picsum.photos/id/${d.length * 3}/200`,
+            creating: true
+        }]);
+    };
+    let completeAnimation = (d, i) => {
+        if (d.deleting) {
+            setData(d => {
+                const result = [...d];
+                result.splice(i, 1);
+                return result;
+            });
+        } else if (d.creating) {
+            setData(d => {
+                const result = [...d];
+                result[i].creating = false;
+                return result;
+            });
+        }
+    };
+    return (
+        <div className="App">
+            <StyleRoot>
+                <p>
+                    Images from&nbsp;
+                    <a href='https://picsum.photos/'>Lorem Picsum</a>
+                </p>
+                <button onClick={createItem}>Add</button>
+                {
+                    data.map((d, i) => <div
+                        style={getStyleForItem(d)}
+                        onAnimationEnd={() => completeAnimation(d, i)}>
+                        <img
+                            id={`image${i}`}
+                            src={d.url}
+                            width={200}
+                            height={200}
+                            alt='Random'
+                            title='Click to delete'
+                            onClick={() => deleteItem(i)}
+                        />
+                    </div>)
+                }
+            </StyleRoot>
+        </div>
+    );
 }
 
 export default App;
--- ch04-07-reanim/src/App.test.js
+++ ch04-07-reanim/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const addButton = getByText(/add/i);
+  expect(addButton).toBeInTheDocument();
 });
