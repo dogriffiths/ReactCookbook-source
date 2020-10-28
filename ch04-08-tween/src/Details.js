afced7bfc368d5b65d35c640854aed574304c00c
David Griffiths <dogriffiths@gmail.com>
2020-10-28 12:13:41 +0000

    feat: Add code for recipe 4-8

---
+++ ch04-08-tween/src/Details.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default ({redTelemetry, blueTelemetry}) => {
+    return <table className='tracking'>
+        <thead>
+        <tr>
+        <th>Vehicle</th>
+        <th>Distance</th>
+        <th>Speed</th>
+        <th>Lap</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Red</td>
+            <td>{(500 * redTelemetry.dist).toFixed(2)}</td>
+            <td>{(500 * redTelemetry.speed).toFixed(2)}</td>
+            <td>{redTelemetry.lap}</td>
+        </tr>
+        <tr>
+            <td>Blue</td>
+            <td>{(500 * blueTelemetry.dist).toFixed(2)}</td>
+            <td>{(500 * blueTelemetry.speed).toFixed(2)}</td>
+            <td>{blueTelemetry.lap}</td>
+        </tr>
+        </tbody>
+    </table>;
+}
