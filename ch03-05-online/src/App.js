43976e46b9d256c817a2288ee7170b6722e05149
David Griffiths <dogriffiths@gmail.com>
2020-10-31 09:38:59 +0000

    fix: Update recipe 3-5 for React 17

--- ch03-05-online/src/App.js
+++ ch03-05-online/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import useOnline from './useOnline';
 import './App.css';
 
--- ch03-05-online/src/App.test.js
+++ ch03-05-online/src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const headingElement = getByText(/network checker/i);
+  render(<App />);
+  const headingElement = screen.getByText(/network checker/i);
   expect(headingElement).toBeInTheDocument();
 });
