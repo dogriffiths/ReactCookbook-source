825f840fd571357df55c95942786004adab42740
David Griffiths <dogriffiths@gmail.com>
2020-10-31 09:57:55 +0000

    fix: Update recipe 3-1 for React 17

--- ch03-01-reducer/src/App.js
+++ ch03-01-reducer/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import Puzzle from "./Puzzle";
 
--- ch03-01-reducer/src/Puzzle.js
+++ ch03-01-reducer/src/Puzzle.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from "react";
+import {useReducer} from "react";
 import reducer from "./reducer";
 
 import "./Puzzle.css";
